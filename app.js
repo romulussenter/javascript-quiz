58a009962d3dfd2c6fd1cf05fb56b8e18967e778
Romulus <romulus.senter@yahoo.com>
2018-04-09 19:07:54 -0400

    Worked on get the the questions populated.

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
 function populate(){
     if(quiz.isEnded())
     {
-        //showScores();  
+        showScores();  
     }
     else{
         //show question
         var element = document.getElementById('question');
         element.innerHTML = quiz.getQuestionIndex().text;
+
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i< choices.length; i++){
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices [i]);
+        }
+    
     }
+};
+
+function guess(id, guess) {
+        var button = document.getElementById(id);
+        button.onclick = function() {
+         quiz.guess(guess);
+         populate(); 
+        }
+}
+
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your score:"+ quiz.score +"</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+
+     
 }
 
 var questions = [
-    new Questions('What is more awesome Judi or Seth?',['Judi','Dont care','Both','Sith'],'Sith'),
+    new Questions('What is more awesome Jedi or Seth?',['Jedi','Dont care','Both','Sith'],'Sith'),
     new Questions('What planet are you from?'['Eath','Mars', 'I have no clue', 'Venous'],'Mars'),
     new Questions('What color is the sky'['Red','Green','Blue','White'],'Red'),
     new Questions('How cool am I?'['Not that cool', 'Pretty awesome','Alright', 'Who are you?'],'Pretty awesome'),
